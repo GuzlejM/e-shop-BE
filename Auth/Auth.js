0741cfea621d4aa4528a9371f21bcd37486d7f0e
GuzlejM <martin.guzlej@gmail.com>
2022-07-27 08:00:15 +0200

    register refactor with b-crypt

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,33 +1,40 @@
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
 // Register a new user
 
 exports.register = async (req, res, next) => {
   const { username, password } = req.body;
-  if (password.length < 6) {
-    return res.status(400).json({ message: "Password less than 6 characters" });
-  }
-  try {
+
+  bcrypt.hash(password, 10).then(async (hash) => {
     await User.create({
       username,
-      password,
-    }).then((user) =>
-      res.status(200).json({
-        message: "User successfully created",
-        user,
-      })
-    );
-  } catch (err) {
-    res.status(401).json({
-      message: "User not successful created",
-      error: error.mesage,
-    });
-  }
+      password: hash,
+    })
+      .then((user) =>
+        res.status(200).json({
+          message: "User successfully created",
+          user,
+        })
+      )
+      .catch((error) =>
+        res.status(401).json({
+          message: "User not successful created",
+          error: error.mesage,
+        })
+      );
+  });
 };
 
 // Login
-
 exports.login = async (req, res, next) => {
+  const { username, password } = req.body;
+  // Check if username and password is provided
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username or Password not present",
+    });
+  }
   try {
     const user = await User.findOne({ username, password });
     if (!user) {
@@ -47,4 +54,61 @@ exports.login = async (req, res, next) => {
       error: error.message,
     });
   }
+};
+
+// Update
+
+exports.update = async (req, res, next) => {
+  const { role, id } = req.body;
+  // Verifying if role and id is presnt
+  if (role && id) {
+    // Verifying if the value of role is admin
+    if (role === "admin") {
+      await User.findById(id)
+        .then((user) => {
+          // Third - Verifies the user is not an admin
+          if (user.role !== "admin") {
+            user.role = role;
+            user.save((err) => {
+              //Monogodb error checker
+              if (err) {
+                res
+                  .status("400")
+                  .json({ message: "An error occurred", error: err.message });
+                process.exit(1);
+              }
+              res.status("201").json({ message: "Update successful", user });
+            });
+          } else {
+            res.status(400).json({ message: "User is already an Admin" });
+          }
+        })
+        .catch((error) => {
+          res
+            .status(400)
+            .json({ message: "An error occurred", error: error.message });
+        });
+    } else {
+      res.status(400).json({
+        message: "Role is not admin",
+      });
+    }
+  } else {
+    res.status(400).json({ message: "Role or Id not present" });
+  }
+};
+
+// Delete user
+exports.deleteUser = async (req, res, next) => {
+  const { id } = req.body;
+  await User.findById(id)
+    .then((user) => user.remove())
+    .then((user) =>
+      res.status(201).json({ message: "User successfully deleted", user })
+    )
+    .catch((error) =>
+      res
+        .status(400)
+        .json({ message: "An error occurred", error: error.message })
+    );
 };
