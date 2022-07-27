1ee21895d3880ac40a1a6d2bad9c34bf215b7365
GuzlejM <martin.guzlej@gmail.com>
2022-07-27 08:24:03 +0200

    jwtoken added

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,5 +1,8 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET;
 
 // Register a new user
 
@@ -11,12 +14,24 @@ exports.register = async (req, res, next) => {
       username,
       password: hash,
     })
-      .then((user) =>
-        res.status(200).json({
+      .then((user) => {
+        const maxAge = 3 * 60 * 60;
+        const token = jwt.sign(
+          { id: user._id, username, role: user.role },
+          jwtSecret,
+          {
+            expiresIn: maxAge, // 3hrs in sec
+          }
+        );
+        res.cookie("jwt", token, {
+          httpOnly: true,
+          maxAge: maxAge * 1000, // 3hrs in ms
+        });
+        res.status(201).json({
           message: "User successfully created",
-          user,
-        })
-      )
+          user: user._id,
+        });
+      })
       .catch((error) =>
         res.status(401).json({
           message: "User not successful created",
@@ -45,12 +60,26 @@ exports.login = async (req, res, next) => {
     } else {
       // comparing given password with hashed password
       bcrypt.compare(password, user.password).then(function (result) {
-        result
-          ? res.status(200).json({
-              message: "Login successful",
-              user,
-            })
-          : res.status(400).json({ message: "Login not successful" });
+        if (result) {
+          const maxAge = 3 * 60 * 60;
+          const token = jwt.sign(
+            { id: user._id, username, role: user.role },
+            jwtSecret,
+            {
+              expiresIn: maxAge, // 3hrs in sec
+            }
+          );
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000, // 3hrs in ms
+          });
+          res.status(201).json({
+            message: "User successfully Logged in",
+            user: user._id,
+          });
+        } else {
+          res.status(400).json({ message: "Login not succesful" });
+        }
       });
     }
   } catch (error) {
