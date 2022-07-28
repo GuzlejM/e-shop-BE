19e39913575a703b65d3828bd49389c4df21f15d
GuzlejM <martin.guzlej@gmail.com>
2022-07-28 13:05:27 +0200

    added verified into userSchema

--- model/User.js
+++ model/User.js
@@ -1,31 +1,12 @@
 const Mongoose = require("mongoose");
 
 const UserSchema = new Mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    minlength: 6,
-    required: true,
-  },
-  confirmPassword: {
-    type: String,
-    minlength: 6,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "Basic",
-    required: true,
-  },
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, minlength: 6, required: true },
+  confirmPassword: { type: String, minlength: 6, required: true },
+  role: { type: String, default: "Basic", required: true },
+  verified: { type: Boolean, default: false },
 });
 
 const User = Mongoose.model("user", UserSchema);
