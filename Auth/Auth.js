0ae39147c63e8c056709cb753b1cd009a7a93001
GuzlejM <martin.guzlej@gmail.com>
2022-07-30 10:24:23 +0200

    added resetPassword, not getting 200

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -146,9 +146,9 @@ exports.register = async (req, res, next) => {
             expiresIn: maxAge, // 3hrs in sec
           }
         );
+        // SEND EMAIL VERIFICATION ON REGISTRATION
         const url = `${process.env.BASE_URL}/verify/${user._id}/${token}`;
         sendEmail(email, "Email Verification", url);
-        console.log(url);
 
         res.cookie("jwt", token, {
           httpOnly: true,
@@ -168,8 +168,69 @@ exports.register = async (req, res, next) => {
   });
 };
 
-// ------------------ Update ------------------
-exports.update = async (req, res, next) => {
+// ------------------SEND RESET PASSWORD ------------------
+exports.sendResetPassword = async (req, res, next) => {
+  const { email } = req.body;
+  // Check if email is provided
+  if (!email) {
+    return res.status(400).json({
+      message: "Email not present",
+    });
+  }
+  await User.findOne({ email })
+    .then((user) => {
+      const maxAge = 3 * 60 * 60;
+      const token = jwt.sign({ id: user._id, username }, jwtSecret, {
+        expiresIn: maxAge, // 3hrs in sec
+      });
+      // SEND EMAIL VERIFICATION ON REGISTRATION
+      const url = `${process.env.BASE_URL}/reset_password/${user._id}/${token}`;
+      resetPassword(email, "Password reset", url);
+
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: maxAge * 1000, // 3hrs in ms
+      });
+      res.status(201).json({
+        message: "A password reset sent to your account",
+        user: user._id,
+      });
+    })
+    .catch((error) =>
+      res.status(401).json({
+        message: "User not successful created",
+        error: error.mesage,
+      })
+    );
+};
+// // ------------------ Update Password ------------------
+// exports.updatePassword = async (req, res, next) => {
+//   const { password, confirmPassword, email } = req.body;
+//   // Check if has password and confirmation password
+//   if (password && confirmPassword) {
+//     // Check if password and confirmation password are the same
+//     if (password !== confirmPassword) {
+//       await User.findOne(email)
+//         .then((user) => {
+
+//         })
+//         .catch((error) => {
+//           res
+//             .status(400)
+//             .json({ message: "An error occurred", error: error.message });
+//         });
+//     } else {
+//       res.status(400).json({
+//         message: "Role is not admin",
+//       });
+//     }
+//   } else {
+//     res.status(400).json({ message: "Missing Password or Confrim Password" });
+//   }
+// };
+
+// ------------------ Update Role ------------------
+exports.updateRole = async (req, res, next) => {
   const { role, id } = req.body;
   // Verifying if role and id is presnt
   if (role && id) {
--- Auth/Route.js
+++ Auth/Route.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, update, deleteUser, adminAuth } = require("./auth");
+const {
+  adminAuth,
+  deleteUser,
+  login,
+  register,
+  sendResetPassword,
+  updateRole,
+} = require("./auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/reset_password").post(sendResetPassword);
 
-router.route("/update").put(adminAuth, update);
+router.route("/updateRole").put(adminAuth, updateRole);
 router.route("/deleteUser").delete(adminAuth, deleteUser);
 
 module.exports = router;
